2d6dbc8a33fa659949ad0d714e2ddb17ab43fb08
Anders Thøgersen <anders@bladre.dk>
2017-05-21 15:19:31 +0200

    table of contents + add object.uml

--- index.sh
+++ index.sh
@@ -14,19 +14,42 @@ cat << EOHEADER
       img {
         margin-left: 2em;
       }
+      h1, h2 {
+        color: #AAA;
+      }
+      h2 {
+        font-size: 120%;
+        margin: 1em 0 0 0;
+      }
+      a {
+        text-decoration: none;
+        color: #7AF;
+      }
+      li {
+        list-style-type: none;
+      }
     </style>
   </head>
   <body>
+  <h1>PlantUML syntax examples</h1>
 EOHEADER
 
-for i in "$@"; do
+echo "<ul>"
+for i in $(echo "$@" | tr ' ' '\n' | sort); do
   baseName=$(echo "$i" | sed -e 's/^\([^.]\+\).*$/\1/')
-  echo "<h3>$baseName</h3>"
-  echo "<table><tr><td>"
+  echo "<li><a href=\"#$baseName\">$baseName</a></li>"
+done
+echo "</ul><hr />"
+
+for i in $(echo "$@" | tr ' ' '\n' | sort); do
+  baseName=$(echo "$i" | sed -e 's/^\([^.]\+\).*$/\1/')
+  echo "<a name=\"$baseName\"></a><table><tr><td>"
+  echo "<h2>$baseName</h2>"
   cat "$i"
   echo "</td><td>"
   echo "<img src=\"${baseName}.png\" />"
   echo "</td></tr></table>"
+  echo "<hr />"
 done
 
 cat << EOFOOTER
